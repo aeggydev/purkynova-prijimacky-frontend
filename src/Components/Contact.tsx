3335d392973ec5147e6b0270d7070399f20786ec
aeggydev <kucernak.a@gmail.com>
2021-12-06 18:15:11 +0100

    Fill text into Contact.tsx

--- src/Components/Contact.tsx
+++ src/Components/Contact.tsx
@@ -1,21 +1,18 @@
-import { Grid, Stack } from "@mui/material";
-import React from "react"
+import { Box, Link, Text } from "@chakra-ui/react";
+import React from "react";
 
 function Contact() {
-  return (
-    <Grid container direction="column" textAlign="center">
-      <Stack spacing={2}>
-        <div>
-          <p>hi</p>
-          <p>hello</p>
-        </div>
-        <div>
-          <p>hi</p>
-          <p>hello</p>
-        </div>
-      </Stack>
-    </Grid>
-  );
+  return <Box>
+    <Text fontWeight="900" mb="1em">Kontaktní osoba</Text>
+    <Text fontWeight="500">Alena Klobásová<br />
+      tel. 541 649 193<br />
+      <Link href="mailto:alena.klobasova@sspbrno.cz" textDecoration="underline">alena.klobasova@sspbrno.cz</Link>
+    </Text>
+    <Text fontWeight="900" my="1em">Organizátor akce</Text>
+    <Text fontWeight="500">Střední průmyslová škola Brno, Purkyňova, příspěvková organizace<br />
+      Purkyňova 97, 612 00 Brno<br />
+      <Link href="http://sspbrno.cz" target="_blank" textDecoration="underline">www.sspbrno.cz</Link></Text>
+  </Box>;
 }
 
 export default Contact;
