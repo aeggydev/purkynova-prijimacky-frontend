77770e0d7b7a991b9335de4681e6e32c69d8c9ca
aeggydev <kucernak.a@gmail.com>
2021-12-13 16:58:23 +0100

    Prepare for switching to a single-grid table layout

--- src/Components/Table.tsx
+++ src/Components/Table.tsx
@@ -2,7 +2,7 @@
 import Settings from "url:/src/Icons/settings.svg";
 // @ts-ignore
 import Mail from "url:/src/Icons/email.svg";
-import { Box, Button, Center, ChakraProps, Grid, GridItem, GridItemProps, GridProps } from "@chakra-ui/react";
+import { Box, Button, ChakraProps, Grid, GridItem, GridItemProps } from "@chakra-ui/react";
 import { DateTime } from "luxon";
 import React, { PropsWithChildren } from "react";
 import { Person } from "../Types/Person";
@@ -10,23 +10,40 @@ import { Person } from "../Types/Person";
 // TODO: Table guide
 // TODO: Use react-table
 
-const colString = "1fr 4fr 4fr 7fr 4fr 2.5fr 2.5fr 2fr 2fr 1fr 1fr"
+const VerticalSplit = (props: PropsWithChildren<ChakraProps>) => (
+  <Grid display="flex" flexDirection="column" {...props}>
+    {props.children}
+  </Grid>
+);
+
+interface HorizontalSplitProps extends ChakraProps {
+  cols: string;
+}
+
+const HorizontalSplit = (props: PropsWithChildren<HorizontalSplitProps>) => (
+  <Box display="grid" gridAutoFlow="column" gridTemplateColumns={props.cols} {...props}>
+    {props.children}
+  </Box>
+);
+
+const colString = "1fr 4fr 4fr 7fr 4fr 2.5fr 2.5fr 2fr 2fr 1fr 1fr";
 
 export function TableHeader({ expanded, bg }: { expanded: boolean, bg: string }) {
   const CellSimple = ({ header, flex = 0 }: { header: string, flex?: number }) => (
     <Box flexGrow={1} border="solid 1px black">
       {header}
     </Box>
-  )
+  );
 
   interface CellProps extends GridItemProps {
   }
+
   const Cell = (props: PropsWithChildren<CellProps>) => (
     <GridItem display="grid" justifyContent="center" alignContent="center"
               border="solid 1px black" fontSize="13px" lineHeight="13px"
               textTransform="uppercase" fontWeight="bold"
               {...props}>{props.children}</GridItem>
-  )
+  );
 
   return expanded
     ? <Box display="flex">
@@ -36,31 +53,34 @@ export function TableHeader({ expanded, bg }: { expanded: boolean, bg: string })
       <CellSimple header="jméno zákon. zást." />
       <CellSimple header="příjmení zákon. zást." />
     </Box>
-    : <Grid bg={bg} templateColumns={colString}
-            templateRows="repeat(2, 1fr)">
-      <Cell rowSpan={2} colStart={1}>ID</Cell>
-      <Cell colStart={2}>jméno, příjmení účastn.</Cell>
-      <Cell colStart={2}>jméno, příjmení zák. zást.</Cell>
-      <Cell rowStart={1} colStart={3} colSpan={3}>základní škola</Cell>
-      <Cell rowStart={2} colStart={3}>telefon</Cell>
-      <Cell rowStart={2} colStart={4}>
-        e-mail zák. zástupce
-      </Cell>
-      <Cell rowStart={2} colStart={5}>ip adresa</Cell>
-      <Cell rowStart={1} rowSpan={2} colStart={6}>
+    : <Grid bg={bg} templateColumns={colString}>
+      <Cell colStart={1}>ID</Cell>
+      <VerticalSplit gridColumnStart={2}>
+        <Cell>jméno, příjmení účastn.</Cell>
+        <Cell>jméno, příjmení zák. zást.</Cell>
+      </VerticalSplit>
+      <VerticalSplit gridColumnStart={3} gridColumnEnd={6}>
+        <Cell flexGrow={1}>základní škola</Cell>
+        <HorizontalSplit cols="4fr 7fr 4fr" flexGrow={1}>
+          <Cell>telefon</Cell>
+          <Cell>e-mail zák. zástupce</Cell>
+          <Cell>ip adresa</Cell>
+        </HorizontalSplit>
+      </VerticalSplit>
+      <Cell colStart={6}>
         variabilní symbol
       </Cell>
-      <Cell rowStart={1} rowSpan={2} colStart={7}>
+      <Cell colStart={7}>
         datum přihlášení
       </Cell>
-      <Cell rowStart={1} rowSpan={2} colStart={8}>
+      <Cell colStart={8}>
         datum splatnosti
       </Cell>
-      <Cell rowStart={1} rowSpan={2} colStart={9}>
+      <Cell colStart={9}>
         datum úhrady
       </Cell>
-      <Cell rowStart={1} rowSpan={2} colStart={10} />
-      <Cell rowStart={1} rowSpan={2} colStart={11}>
+      <Cell colStart={10} />
+      <Cell colStart={11}>
         EXP
       </Cell>
     </Grid>
@@ -70,19 +90,20 @@ export function Table({ people }: { people: Person[] }) {
   return <Box>
     <TableHeader expanded={false} bg="white" />
     {people.map(person => <TableRow person={person} bg="white" expanded={false} />)}
-  </Box>
+  </Box>;
 }
 
 export function TableRow(props: { person: Person, bg: string, expanded: boolean }) {
-  const signInDate = DateTime.fromISO(props.person.signInDate).toFormat("dd. MM. yyyy hh:mm")
-  const payTillDate = DateTime.fromISO(props.person.payTillDate).toFormat("dd. MM. yyyy")
+  const signInDate = DateTime.fromISO(props.person.signInDate).toFormat("dd. MM. yyyy hh:mm");
+  const payTillDate = DateTime.fromISO(props.person.payTillDate).toFormat("dd. MM. yyyy");
   const paidDate = props.person.paidDate
     ? DateTime.fromISO(props.person.paidDate).toFormat("dd. MM. yyyy")
-    : "...nothing"
+    : "...nothing";
 
   interface CellProps extends GridItemProps {
-    bold?: boolean
+    bold?: boolean;
   }
+
   const Cell = (props: PropsWithChildren<CellProps>) => (
     <GridItem border="solid 1px black" fontSize="13px"
               display="grid" justifyContent="center" alignContent="center"
@@ -90,40 +111,45 @@ export function TableRow(props: { person: Person, bg: string, expanded: boolean 
               {...props}>
       {props.children}
     </GridItem>
-  )
+  );
 
-  return <Grid bg={props.bg} templateColumns={colString}
-               templateRows="repeat(3, 1fr)">
+  return <Grid bg={props.bg} templateColumns={colString}>
     <Cell rowSpan={2} colStart={1} bold fontSize="14px">
       {props.person.personalId}
     </Cell>
-    <Cell colStart={2} bold>{props.person.applicantName} {props.person.applicantSurname}</Cell>
-    <Cell colStart={2}>{props.person.parentName} {props.person.parentSurname}</Cell>
-    <Cell rowStart={1} colStart={3} colSpan={3} bold color="gray">{props.person.schoolName}</Cell>
-    <Cell rowStart={2} colStart={3} bold>{props.person.phone}</Cell>
-    <Cell rowStart={2} colStart={4} bold>
-      {props.person.parentEmail}
-    </Cell>
-    <Cell rowStart={2} colStart={5} bold color="gray">{props.person.ip}</Cell>
-    <Cell rowStart={1} rowSpan={2} colStart={6} bold>
+    <VerticalSplit gridColumn={2}>
+      <Cell bold>{props.person.applicantName} {props.person.applicantSurname}</Cell>
+      <Cell>{props.person.parentName} {props.person.parentSurname}</Cell>
+    </VerticalSplit>
+    <VerticalSplit gridColumnStart={3} gridColumnEnd={6}>
+      <Cell bold color="gray">{props.person.schoolName}</Cell>
+      <HorizontalSplit cols="4fr 7fr 4fr">
+        <Cell bold>{props.person.phone}</Cell>
+        <Cell bold>
+          {props.person.parentEmail}
+        </Cell>
+        <Cell bold color="gray">{props.person.ip}</Cell>
+      </HorizontalSplit>
+    </VerticalSplit>
+    <Cell colStart={6} bold>
       {props.person.variableSymbol}
     </Cell>
-    <Cell rowStart={1} rowSpan={2} colStart={7} bold color="gray">
+    <Cell colStart={7} bold color="gray">
       {signInDate}
     </Cell>
-    <Cell rowStart={1} rowSpan={2} colStart={8} bold>
+    <Cell colStart={8} bold>
       {payTillDate}
     </Cell>
-    <Cell rowStart={1} rowSpan={2} colStart={9} bold>
+    <Cell colStart={9} bold>
       {paidDate}
     </Cell>
-    <Cell rowStart={1} rowSpan={2} colStart={10} bold overflow="hidden">
+    <Cell colStart={10} bold overflow="hidden">
       <Button bg="rgb(185, 28, 28)" rounded="0.25rem" h="60%" w="100%" display="flex" justifyContent="center"
               alignItems="center">
         <img src={Mail} alt="Mail icon" />
       </Button>
     </Cell>
-    <Cell rowStart={1} rowSpan={2} colStart={11} overflow="hidden">
+    <Cell colStart={11} overflow="hidden">
       <Button h="60%" w="100%" bg="transparent">
         <img src={Settings} alt="Settings icon" /> {/* TODO: Fix color */}
       </Button>
