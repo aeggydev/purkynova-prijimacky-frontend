eff60e0cb6dc479f59f1041c730dd3ef5e7d2757
aeggydev <kucernak.a@gmail.com>
2021-12-19 13:24:43 +0100

    Make Header.tsx use an SVG icon

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,32 +3,30 @@ import spsLogo from "url:/src/Icons/sspbrno.png"
 import React from "react"
 import { LightText, TopbarBg } from "../theme"
 import Menubar from "./Menubar"
-import { Box } from "@chakra-ui/react"
+import { Box, Link } from "@chakra-ui/react"
+import Sps from "../Icons/Sps"
 
 function Header() {
   return (
     <Box background={TopbarBg} color={LightText}>
-      <a
-        href="http://sspbrno.cz"
-        target="_blank"
-        rel="noreferrer"
-        style={{
-          width: "100%", height: "8rem", background: TopbarBg, boxSizing: "border-box",
-          alignItems: "center", display: "flex", flexDirection: "row", padding: "0 9rem"
-        }}
-      >
-        <img
-          src={spsLogo}
-          alt="Logo školy"
-          style={{ height: "auto", width: "auto", maxHeight: "5rem" }}
-        />
-        <div style={{ marginLeft: "3rem", display: "flex", flexDirection: "column" }}>
-        <span style={{ fontSize: "1.25rem", lineHeight: "1.75rem", marginBottom: "0.125rem", display: "block" }}>
-          Střední průmyslová škola Brno, Purkyňova, příspěvková organizace
-        </span>
-          <span style={{ fontSize: "1.5rem", lineHeight: "2rem" }}>PŘÍJMAČKY NANEČISTO</span>
-        </div>
-      </a>
+      <Box w="100%" h="8rem" bg={TopbarBg} boxSizing="border-box"
+           alignItems="center"
+           display="flex" flexDir="row"
+           p="0 9rem">
+        <Link href="http://sspbrno.cz"
+              target="_blank"
+              rel="noreferrer"
+              w="94px"
+              display="flex">
+          <Sps color="white" />
+        </Link>
+        <Box ml="3rem" d="flex" flexDir="column">
+          <Box fontSize="1.25rem" lineHeight="1.75rem" mb="0.125rem" d="block">
+            Střední průmyslová škola Brno, Purkyňova, příspěvková organizace
+          </Box>
+          <Box as="span" fontSize="1.5rem" lineHeight="2rem">PŘÍJMAČKY NANEČISTO</Box>
+        </Box>
+      </Box>
       <Menubar />
     </Box>
   )
