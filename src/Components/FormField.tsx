ea1223c547f6c9b5e45f1267044f5279f1c2db11
aeggydev <kucernak.a@gmail.com>
2022-03-20 19:29:00 +0100

    Use context for state in form

--- src/Components/FormField.tsx
+++ src/Components/FormField.tsx
@@ -1,18 +1,27 @@
 import { ChakraProps, Input } from "@chakra-ui/react"
+import { FormContext, StateDefault } from "./InfoForm"
+import { ChangeEvent, useContext } from "react"
 
 interface Props extends ChakraProps {
   label: string;
+  fieldName: keyof typeof StateDefault;
 }
 
 const FormField = (props: Props) => {
-  return <Input bg="white" shadow="md" mb="1em" placeholder={props.label}
-                {...props} />
-  {/*
-    <Text position="absolute" zIndex="1" right="3" bottom="0"
-          userSelect="none"
-          textTransform="uppercase" fontWeight="500">{props.label}</Text>
-*/
+  const [state, setState] = useContext(FormContext)
+  // @ts-ignore
+  const value = state[props.fieldName]
+  function onChange(e: ChangeEvent<HTMLInputElement>) {
+    console.log(e.target.value)
+    console.log(state)
+    const change = {} as any
+    change[props.fieldName] = e.target.value
+    // @ts-ignore
+    setState({...state, ...change})
   }
+
+  return <Input bg="white" shadow="md" mb="1em" placeholder={props.label}
+                value={value} onChange={onChange} />
 }
 
 export default FormField
--- src/Components/InfoForm.tsx
+++ src/Components/InfoForm.tsx
@@ -6,41 +6,57 @@ import { FormSubmitBg, TopbarDarkBg } from "../theme"
 import React, { useState } from "react"
 import { Row } from "./Views/Form"
 
-export default function InfoForm() {
-  useState("")
+export const StateDefault = {
+  participantName: "",
+  participantSurname: "",
+  schoolName: "",
+  parentName: "",
+  parentSurname: "",
+  parentEmail: "",
+  parentPhone: ""
+}
+export const FormContext = React.createContext([StateDefault, (x: typeof StateDefault) => {}]);
 
-  return <ShadowBox mx="4em" position="relative" alignContent="center">
-    <InfoButton icon={IconType.questionMark} position="absolute" top="10px" right="10px">
-      V případě jakýchkoliv problémů s přihláškou nás neváhejte <u>kontaktovat</u>.
-    </InfoButton>
-    <Box display="grid" gridTemplateColumns="1fr 5%">
-      <Row row={1}>
-        <FormField label="Jméno účastníka" />
-        <FormField label="Příjmení účastníka" />
-      </Row>
-      <Row row={2}>
-        <FormField label="Základní škola, obec" mb="2.5em" />
-      </Row>
-      <Row row={3}>
-        <FormField label="Jméno zákonného zástupce" />
-        <FormField label="Příjmení zákonného zástupce" />
-      </Row>
-      <InfoButton icon={IconType.exclamationPoint} pt="0.5em" gridRow={3}>
-        V případě, že už jste plnoletí, vyplňte do polí pro zákonného zástupce své jméno, e&#8209;mail a telefonní
-        číslo. {/* TODO: replace escape sequence with css solution */}
+export default function InfoForm() {
+  const [state, setState] = useState(StateDefault)
+  function submit() {
+    console.log(state)
+  }
+
+  return <FormContext.Provider value={[state, setState]}>
+    <ShadowBox mx="4em" position="relative" alignContent="center">
+      <InfoButton icon={IconType.questionMark} position="absolute" top="10px" right="10px">
+        V případě jakýchkoliv problémů s přihláškou nás neváhejte <u>kontaktovat</u>.
       </InfoButton>
-      <Row row={4}>
-        <FormField label="E-Mail zákonného zástupce" />
-      </Row>
-      <Row row={5}>
-        <FormField label="Telefonní číslo zákonného zástupce" />
-        {/* TODO: add checking of phone numbers */}
-        {/* TODO: should automatically fill in +420 */}
-      </Row>
-      <InfoButton icon={IconType.exclamationPoint} pt="0.5em" gridRow={5}>
-        Telefonní číslo je pouze sekundární forma komunikace. Je možné zadat pouze čísla s českou předvolbou +420.
-      </InfoButton>
-    </Box>
-    <Button bg={FormSubmitBg} mt="2em" _hover={{ bg: TopbarDarkBg }} color="white">ODESLAT PŘIHLÁŠKU</Button>
-  </ShadowBox>
+      <Box display="grid" gridTemplateColumns="1fr 5%">
+        <Row row={1}>
+          <FormField label="Jméno účastníka" fieldName="participantName"  />
+          <FormField label="Příjmení účastníka" fieldName="participantSurname" />
+        </Row>
+        <Row row={2}>
+          <FormField label="Základní škola, obec" mb="2.5em" fieldName="schoolName" />
+        </Row>
+        <Row row={3}>
+          <FormField label="Jméno zákonného zástupce" fieldName="parentName" />
+          <FormField label="Příjmení zákonného zástupce" fieldName="parentSurname" />
+        </Row>
+        <InfoButton icon={IconType.exclamationPoint} pt="0.5em" gridRow={3}>
+          V případě, že už jste plnoletí, vyplňte do polí pro zákonného zástupce své jméno, e&#8209;mail a telefonní
+          číslo. {/* TODO: replace escape sequence with css solution */}
+        </InfoButton>
+        <Row row={4}>
+          <FormField label="E-Mail zákonného zástupce" fieldName="parentEmail" />
+        </Row>
+        <Row row={5}>
+          <FormField label="Telefonní číslo zákonného zástupce" fieldName="parentPhone" />
+          {/* TODO: add checking of phone numbers */}
+          {/* TODO: should automatically fill in +420 */}
+        </Row>
+        <InfoButton icon={IconType.exclamationPoint} pt="0.5em" gridRow={5}>
+          Telefonní číslo je pouze sekundární forma komunikace. Je možné zadat pouze čísla s českou předvolbou +420.
+        </InfoButton>
+      </Box>
+      <Button bg={FormSubmitBg} mt="2em" _hover={{ bg: TopbarDarkBg }} color="white" onClick={submit}>ODESLAT PŘIHLÁŠKU</Button>
+    </ShadowBox>
+  </FormContext.Provider>
 }
