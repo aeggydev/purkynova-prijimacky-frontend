5c02e56512833ffb27ecbb845190b3b4cdca4aa1
aeggydev <kucernak.a@gmail.com>
2022-03-26 12:49:40 +0100

    Make all svg parameters in react icons camelcase

--- src/Icons/CloseTable.tsx
+++ src/Icons/CloseTable.tsx
@@ -1,5 +1,5 @@
 export default ({ color }: { color: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24"
+  <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24"
        height="24px" viewBox="0 0 24 24" width="24px" fill={color}>
     <rect fill="none" height="24" width="24" />
     <path
--- src/Icons/Filter.tsx
+++ src/Icons/Filter.tsx
@@ -1,5 +1,5 @@
 export default ({ color }: { color: string }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24"
+  <svg xmlns="http://www.w3.org/2000/svg" enableBackground="new 0 0 24 24" height="24px" viewBox="0 0 24 24"
        width="24px" fill={color}>
     <g>
       <path d="M0,0h24 M24,24H0" fill="none" />
