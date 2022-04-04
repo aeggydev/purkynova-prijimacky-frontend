875588409cc44c1db4d12f4b1b996aa0ff2068b7
aeggydev <kucernak.a@gmail.com>
2022-04-04 18:14:13 +0200

    Implement functionality in RegisterParticipant.tsx

--- src/Components/AdminViews/Dashboard/ButtonArea/Modals/RegisterParticipant.tsx
+++ src/Components/AdminViews/Dashboard/ButtonArea/Modals/RegisterParticipant.tsx
@@ -15,40 +15,64 @@ import {
 import { EmailIcon, PhoneIcon } from "@chakra-ui/icons"
 import styled from "styled-components"
 import { useRef } from "react"
+import { GetParticipantsDocument, NewParticipantInput, useNewParticipantMutation } from "../../../../../graphql/graphql"
+import { SubmitHandler, useForm } from "react-hook-form"
 
 interface IProps {
     setStatus: (newState: boolean) => void // Close or open the modal
     isOpen: boolean
 }
 
+interface FormFields extends NewParticipantInput {
+}
+
 export function RegisterParticipant({ isOpen, setStatus }: IProps) {
-    const initialRef = useRef<any>()
+    const initialFocusRef = useRef<any>()
+    const { register, handleSubmit, watch, formState: { errors }, reset } = useForm<FormFields>()
+    const [newParticipant] = useNewParticipantMutation({
+        refetchQueries: [GetParticipantsDocument]
+    })
+    const { ref: partNameRef, ...partNameRest } = register("participantName")
 
     function onClose() {
         setStatus(false)
     }
 
+    const onSubmit: SubmitHandler<FormFields> = async data => {
+        const returned = await newParticipant({ variables: { addParticipant: data } })
+        setStatus(false)
+        reset()
+    }
+
     return (
         <Modal isOpen={isOpen} onClose={onClose}
-               initialFocusRef={initialRef}>
+               initialFocusRef={initialFocusRef}>
             <ModalOverlay />
             <ModalContent>
                 <ModalHeader>Přidat účastníka</ModalHeader>
                 <ModalCloseButton />
-                <ModalBody>
-                    <SForm>
+                <ModalBody display="grid" gridGap="1ex">
+                    <SForm onSubmit={handleSubmit(onSubmit)}>
                         <Grid templateColumns="1fr 1fr">
                             <Input placeholder="Jméno účastníka"
-                                   ref={initialRef}
+                                   {...partNameRest}
+                                   ref={(e) => {
+                                       partNameRef(e)
+                                       initialFocusRef.current = e
+                                   }}
                                    roundedRight="none" />
                             <Input placeholder="Příjmení účastníka"
+                                   {...register("participantSurname")}
                                    roundedLeft="none" />
                         </Grid>
-                        <Input placeholder="Základní škola, obec" />
+                        <Input placeholder="Základní škola, obec"
+                               {...register("school")} />
                         <Grid templateColumns="1fr 1fr">
                             <Input placeholder="Jméno zák. zást."
+                                   {...register("parentName")}
                                    roundedRight="none" />
                             <Input placeholder="Příjmení zák. zást."
+                                   {...register("parentSurname")}
                                    roundedLeft="none" />
                         </Grid>
                         <InputGroup>
@@ -56,21 +80,23 @@ export function RegisterParticipant({ isOpen, setStatus }: IProps) {
                                 pointerEvents="none"
                                 children={<PhoneIcon color="gray.600" />}
                             />
-                            <Input type="email" placeholder="E-Mail zákonného zástupce" />
+                            <Input type="email" placeholder="E-Mail zákonného zástupce"
+                                   {...register("email")} />
                         </InputGroup>
                         <InputGroup>
                             <InputLeftElement
                                 pointerEvents="none"
                                 children={<EmailIcon color="gray.600" />}
                             />
-                            <Input placeholder="Telefonní číslo zákonného zástupce" />
+                            <Input placeholder="Telefonní číslo zákonného zástupce"
+                                   {...register("phone")} />
                         </InputGroup>
                         {/* Add other props useful to the admin */}
                     </SForm>
                 </ModalBody>
 
                 <ModalFooter display="flex">
-                    <Button flexGrow={1} mr={3}>Registrovat</Button>
+                    <Button onClick={() => handleSubmit(onSubmit)()} flexGrow={1} mr={3}>Registrovat</Button>
                     <Button onClick={onClose}>Zavřít</Button>
                 </ModalFooter>
             </ModalContent>
