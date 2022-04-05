b7f818d06006a9639c7016bdaa79671f6977f257
aeggydev <kucernak.a@gmail.com>
2022-04-05 12:44:43 +0200

    Add a basic loading skeleton to InfoPanels.tsx

--- src/Components/AdminViews/Dashboard/InfoPanels.tsx
+++ src/Components/AdminViews/Dashboard/InfoPanels.tsx
@@ -1,15 +1,26 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react"
+import { Box, Grid, GridItem, Skeleton } from "@chakra-ui/react"
 import React, { useContext } from "react"
 import ShadowBox from "../../Containers/ShadowBox"
 import { useGetSettingsQuery, useGetStatisticsQuery } from "../../../graphql/graphql"
 import { ReporterContext } from "../../Error/Reporter"
 
-function InfoBoxRow({ left, right }: { left: string | number, right: string | number }) {
+function InfoBoxRow({ left, right }: { left: string | number, right: string | number | undefined }) {
+    const context = useContext(InfoContext)
+    console.log(context)
+
     return <Grid templateColumns="1fr 1fr" fontWeight="bold" lineHeight="30px">
         <GridItem justifySelf="start">{left}</GridItem>
-        <GridItem justifySelf="end">{right}</GridItem>
+        <GridItem justifySelf="end" display="flex" alignItems="center">
+            <Skeleton minHeight="70%" minWidth="1.5em" isLoaded={!context.loading}>{right}</Skeleton>
+        </GridItem>
     </Grid>
 }
+
+interface IInfoContext {
+    loading: boolean
+}
+
+const InfoContext = React.createContext<IInfoContext>({} as IInfoContext)
 
 export function InfoPanels() {
     const reporter = useContext(ReporterContext)
@@ -30,26 +41,31 @@ export function InfoPanels() {
         fetchPolicy: "cache-and-network",
         onError: error => reporter.error("Problém při spojení", error.message)
     })
-    if (settingsError || statisticsError) return <div>Error: {settingsError?.message ?? statisticsError?.message}</div>
-    if (settingsLoading || statisticsLoading) return <div>Loading InfoPanels</div>
+    const settings = settingsData?.settings
+    const statistics = statisticsData?.statistics
 
-    const settings = settingsData!.settings
-    const statistics = statisticsData!.statistics
+    const contextValue: IInfoContext = {
+        loading: settingsLoading || statisticsLoading
+    }
 
-    return (
+    const capacity = (settings?.capacity && settings?.allowedOver)
+        ? settings?.capacity + settings?.allowedOver
+        : undefined
+
+    return <InfoContext.Provider value={contextValue}>
         <Box d="flex" flexDir="row" justifyContent="space-between" alignItems="start"
              px="5rem">
             <ShadowBox p=".75rem">
-                <InfoBoxRow left="Maximální kapacita" right={settings.capacity} />
-                <InfoBoxRow left="Povolená místa pod čarou" right={settings.allowedOver} />
-                <InfoBoxRow left="Celkový počet míst" right={settings.capacity + settings.allowedOver} />
-                <InfoBoxRow left="Přihlášení v provozu" right={settings.signUpAllowed ? "Ano" : "Ne"} />
+                <InfoBoxRow left="Maximální kapacita" right={settings?.capacity} />
+                <InfoBoxRow left="Povolená místa pod čarou" right={settings?.allowedOver} />
+                <InfoBoxRow left="Celkový počet míst" right={capacity} />
+                <InfoBoxRow left="Přihlášení v provozu" right={settings?.signUpAllowed ? "Ano" : "Ne"} />
             </ShadowBox>
             <ShadowBox p=".75rem">
-                <InfoBoxRow left="Celkem přihlášek" right={statistics.totalSignups} />
-                <InfoBoxRow left="Zbývající kapacita" right={statistics.remainingCapacity} />
-                <InfoBoxRow left="Zbývající místa pod čarou" right={statistics.remainingCapacityOver} />
-                <InfoBoxRow left="Přihlášek odstraněno" right={statistics.removedSignups} />
+                <InfoBoxRow left="Celkem přihlášek" right={statistics?.totalSignups} />
+                <InfoBoxRow left="Zbývající kapacita" right={statistics?.remainingCapacity} />
+                <InfoBoxRow left="Zbývající místa pod čarou" right={statistics?.remainingCapacityOver} />
+                <InfoBoxRow left="Přihlášek odstraněno" right={statistics?.removedSignups} />
             </ShadowBox>
             <ShadowBox p=".75rem">
                 <InfoBoxRow left="Platné" right="6" />
@@ -60,5 +76,5 @@ export function InfoPanels() {
                 <InfoBoxRow left="Bez e-mailu o posunu nad čáru" right="0" />
             </ShadowBox>
         </Box>
-    )
+    </InfoContext.Provider>
 }
