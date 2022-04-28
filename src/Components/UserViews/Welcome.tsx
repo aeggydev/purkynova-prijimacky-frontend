d6b11c48e57ca1a57c88d2245847d465a511de74
aeggydev <kucernak.a@gmail.com>
2022-04-28 18:58:22 +0200

    Fix formatting of code in Welcome.tsx

--- src/Components/UserViews/Welcome.tsx
+++ src/Components/UserViews/Welcome.tsx
@@ -5,9 +5,9 @@ import ShadowBox from "../Containers/ShadowBox"
 import ApplicationCount from "./Reusable/ApplicationCount"
 
 const Paragraph = styled.p`
-  margin-bottom: 1em;
-  font-weight: 500;
-  line-height: 24px;
+    margin-bottom: 1em;
+    font-weight: 500;
+    line-height: 24px;
 `
 
 // TODO: Add support for narrower layouts
@@ -15,15 +15,17 @@ export default function Welcome() {
     return <ContentContainer>
         <ApplicationCount />
         <ShadowBox>
-            <Paragraph style={{ fontWeight: "bold", fontSize: "22px", marginBottom: "2em" }}>Přijímačky na střední
-                školu? Bez
-                obav.</Paragraph>
+            <Paragraph style={{ fontWeight: "bold", fontSize: "22px", marginBottom: "2em" }}>
+                Přijímačky na střední školu? Bez obav.
+            </Paragraph>
             <Paragraph>Připravíme Vás na CERMAT zkoušky v roce 2023.</Paragraph>
-            <Paragraph>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et
+            <Paragraph>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
                 dolore
-                magna aliqua. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat.</Paragraph>
-            <Paragraph>Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Vel facilisis volutpat est velit
+                magna aliqua. Egestas quis ipsum suspendisse ultrices gravida dictum fusce ut placerat.
+            </Paragraph>
+            <Paragraph>
+                Faucibus in ornare quam viverra orci sagittis eu volutpat odio. Vel facilisis volutpat est velit
                 egestas dui id
                 ornare arcu. Pulvinar etiam non quam lacus suspendisse faucibus interdum. Lorem mollis aliquam ut
                 porttitor leo.
@@ -31,10 +33,12 @@ export default function Welcome() {
                 gravida.
                 Commodo
                 elit at imperdiet dui. Facilisi etiam dignissim diam quis. Sapien nec sagittis aliquam
-                malesuada.</Paragraph>
-            <Paragraph>Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Cursus metus aliquam
-                eleifend
-                mi.</Paragraph>
+                malesuada.
+            </Paragraph>
+            <Paragraph>
+                Cursus metus aliquam eleifend mi in nulla posuere sollicitudin aliquam. Cursus metus aliquam eleifend
+                mi.
+            </Paragraph>
         </ShadowBox>
     </ContentContainer>
 }
